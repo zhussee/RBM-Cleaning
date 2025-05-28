5d2cbe6d56a8976e906907c5e6829d61ce506ac6
Bekzat Yussubayev <beka.akzhulov@gmail.com>
2025-05-28 06:52:21 +0500

    url appdate

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+def default_avatar_path():
+    return 'avatars/default.png'  
+
 class UserProfile(models.Model):
     STATUS_CHOICES = [
         ('user', 'Клиент'),
@@ -13,7 +17,12 @@ class UserProfile(models.Model):
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     company = models.ForeignKey("backend_company.CleaningCompany", null=True, blank=True, on_delete=models.SET_NULL)
     is_verified = models.BooleanField(default=False)
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        null=True,
+        blank=True,
+        default=default_avatar_path  # вот эта строка — ключевая
+    )
 
     def __str__(self):
         return f"{self.user.email} ({self.get_status_display()})"
